134a2d00352f83cb35e0803705f983bb2275d3a8
ISMAILZNIGUEF <ismailzniguef@gmail.com>
2021-08-06 19:50:13 +0100

    Deleted the file from the git repository

--- main.js
+++
@@ -1,9 +0,0 @@
-
-let test = document.getElementById("test");
-
-// Ce gestionnaire ne sera exécuté qu'une fois
-// lorsque le curseur se déplace sur la liste
-test.addEventListener("mouseenter", function( event ) {
-  // on met l'accent sur la cible de mouseenter
-  event.target.style.display = "block";
-}, false);
